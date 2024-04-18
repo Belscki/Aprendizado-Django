5808001e820231a3134c3f5b7c4b2207ed408cec
Belscki <136617733+Belscki@users.noreply.github.com>
2024-04-18 11:55:03 -0300

    v1.02

--- blog/views.py
+++ blog/views.py
@@ -22,6 +22,39 @@ posts = [
     }
 ]
 
+links =[
+    {
+        'video' : 'https://youtube.com/playlist?list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p&si=CM0zas3kZVLRh7Yk'
+    },
+    {
+        'video' : 'https://www.youtube.com/live/c0x_AaPjNCY?si=zFmm318lPWe6Mctk'
+    },
+    {
+        'video' : 'https://youtu.be/s7aINQPGNDM?si=XPcy-ZqNWs0o5Cil'
+    },
+    {
+        'video' : 'https://youtu.be/fHpiwPcTcYw?si=xHWe-tzUaehI6p4h'
+    },
+    {
+        'video' : 'https://youtu.be/i5JykvxUk_A?si=iCHTNB71AojqNLQS'
+    },
+    {
+        'video' : 'https://youtu.be/JIFqqdRxmVo?si=CyAvsYpZ8VzLAMu1'
+    },
+    {
+        'video' : 'https://youtu.be/tujhGdn1EMI?si=VWmWfpxKgJ3rfuQB'
+    },
+    {
+        'video' : 'https://youtu.be/c708Nf0cHrs?si=lOCn05mWkl7bO6j6'
+    },
+    {
+        'video' : 'https://youtube.com/playlist?list=PLsTx8TSx2fHY01FnuxBdwiBiOdZdPGik7&si=jkfJqIZs1VWYatvA'
+    },
+    {
+        'video' : 'https://youtu.be/pvh_UPHuz9U?si=DXYBej9uNbhTMp5c'
+    }
+]
+
 
 def home(request):
     context = {
